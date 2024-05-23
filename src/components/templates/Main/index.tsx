b9266a956f4cee6f358b4a1afcb5557bc6b63367
Rafael Kramer da Silva <rafael.silva@grupom8.local>
2024-05-23 08:22:02 -0300

    create link button

--- src/components/templates/Main/index.tsx
+++ src/components/templates/Main/index.tsx
@@ -4,6 +4,18 @@ import * as S from './styles'
 export const MainTemplate = () => {
   return (
     <S.MainTemplate>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '8px',
+        }}
+      >
+        <a href="/cadastro" style={{ color: 'red' }}>
+          Clique aqui para ir para o cadastro
+        </a>
+      </div>
       <Outlet />
     </S.MainTemplate>
   )
